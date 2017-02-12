656cce9145f7e2e527dbcfe05b83339b5562e8f4
James Tapsell <email@jrtapsell.co.uk>
2017-02-12 20:53:25 +0000

    Updated with rebuilt class

--- plugin/src/jsonviewer/setup/DocumentSetupParticipant.java
+++ plugin/src/jsonviewer/setup/DocumentSetupParticipant.java
@@ -3,6 +3,7 @@ package jsonviewer.setup;
 import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentExtension3;
+import json.utils.LocatedJsonException;
 
 public class DocumentSetupParticipant implements IDocumentSetupParticipant {
 
